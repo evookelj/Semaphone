8e4f4405be1a1954960e27ce1c7bf4bb3255ce90
eVookelj <evukelj@stuy.edu>
2016-12-10 12:58:50 -0500

    Doesn't flat out crash anymore

--- telephone.c
+++ telephone.c
@@ -7,6 +7,7 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <string.h>
+#include <errno.h>
 
 char* getLastLine(char* filename, int shmkey) {
   int f = open(filename, O_RDONLY, 0);
@@ -21,28 +22,33 @@ char* getLastLine(char* filename, int shmkey) {
 }
 
 void getUserLine(char* filename, int shmkey, int semkey) {
-  char* line;
-  scanf("What line would you like to add?\n%s", line);
-  int shmid = shmget(shmkey,0,0);
-  int semid = semget(semkey,0,0);
+  char newLine[10000];
+  printf("Your turn!\n");
+  fgets(newLine, sizeof(newLine), stdin);
 
+  //get sem and shm
+  int shmid = shmget(shmkey,1,0);
+  int semid = semget(semkey,1,0);
+  printf("semid: %d\nshmid: %d\n", semid, shmid);
+
+  //take semaphore
   struct sembuf sb;
   sb.sem_num = 0;
   sb.sem_flg = SEM_UNDO;
   sb.sem_op = -1;
-
   semop(semid, &sb, 1);
 
-  int newSize;
-  void* att = shmat(shmid, &newSize, 0);
-  printf("result shmat: %d\n", att);
-  newSize = strlen(line);
-  shmdt(&newSize);
-  printf("after shmdt\n");
+  //write new length to shm
+  int* size_ptr = (int*) shmat(shmid, 0, 0);
+  int new_size = *size_ptr;
+  new_size= strlen(newLine);
+  shmdt(size_ptr);
 
+  //write to file
   int fd = open(filename, O_WRONLY | O_APPEND, 0);
-  write(fd, line, newSize);
-  
+  write(fd, newLine, new_size);
+
+  //give back sem
   sb.sem_op = 1;
   semop(semid, &sb, 1);
 
@@ -55,7 +61,8 @@ int main() {
   int shm_key = ftok("resources.c", 7);
   int sem_key = ftok("resources.c", 8);
   char* ll = getLastLine(filename, shm_key);
-  getUserLine(filename, shm_key, sem_key);
+  printf("Last: %s\n", ll);
+  //getUserLine(filename, shm_key, sem_key);
   
   return 0;
 }
